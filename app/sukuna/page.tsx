e63d6603dd75a0fb1581e8ba06526417da6dd97c
Usaib <ahmedusaib@gmail.com>
2026-03-08 17:52:54 +0500

    code for gesture detection

--- app/sukuna/page.tsx
+++ app/sukuna/page.tsx
@@ -4,10 +4,21 @@
 
 
 
+"use client"
+
+import HandTracker from "@/components/HandTracker"
+
 export default function SukunaDomainExpansion() {
-    return(
-        <div>
-            <p>RYOIKI TENKAI</p>
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
+      <h1 className="text-4xl font-bold mb-6">RYOIKI TENKAI</h1>
+
+      <HandTracker
+        onGestureDetected={() => {
+          console.log("Sukuna Domain Expansion Activated!")
+          // Here later you can trigger your animation/effect
+        }}
+      />
+    </div>
+  )
 }
