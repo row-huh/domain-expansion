0a0f8f6e3e9e41624da6dada5c9908960873ba05
Roha Pathan <rohaabropathan@gmail.com>
2026-03-09 00:37:48 +0500

    (init) hand tracking

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ["@mediapipe/hands", "@mediapipe/camera_utils"],
 };
 
 export default nextConfig;
