1f63e59265784bb35d7e04bfa5d8b2f068cd5b77
Roha Pathan <rohaabropathan@gmail.com>
2026-03-14 00:31:52 +0500

    removed redundant routes + updated page.tsx

--- app/gojo/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import CameraWithHandTracker from "@/components/CameraWithHandTracker"
-
-export default function Page() {
-  return <CameraWithHandTracker onHandsDetected={(hands) => console.log(hands)} />
-}
